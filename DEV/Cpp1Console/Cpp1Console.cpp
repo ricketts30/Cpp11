d57ce14eeb07df029e2b3fdeebd220da77e1e838
predication <ricketts30@yahoo.com[user]>
2013-03-25 20:59:06 +0000

    added a basic console app

---
+++ DEV/Cpp1Console/Cpp1Console.cpp
@@ -0,0 +1,18 @@
+// Cpp1Console.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	printf("\n(*********************)");
+	printf("\n(* C++11 Experiments *)");
+	printf("\n(*********************)");
+	
+	printf("\n\nPress ENTER to Quit.");
+	char c[1];
+	gets_s(c, 1);
+	return 0;
+}
+
